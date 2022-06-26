b20ec9b7b0e978c9f9d1bb63ca71557fa90f64a1
MateCon <68882595+MateCon@users.noreply.github.com>
2022-06-26 17:55:44 -0300

    equipo de mate - version 2

--- Assets/Teams/ORTcoderMate/PlayerOne.cs
+++ Assets/Teams/ORTcoderMate/PlayerOne.cs
@@ -7,15 +7,21 @@ namespace Teams.ORTcoderMate
 {
     public class PlayerOne : TeamPlayer
     {
-        public bool IsBallNearOwnGoal()
+        public bool IsNearest()
         {
-            return Vector3.Distance(GetMyGoalPosition(), GetBallPosition()) < 4.0f;
+            float distance = Vector3.Distance(GetPosition(), GetBallPosition());
+            float distance2 = Vector3.Distance(GetTeamMatesInformation()[0].Position, GetBallPosition());
+            float distance3 = Vector3.Distance(GetTeamMatesInformation()[1].Position, GetBallPosition());
+            float distance4 = Vector3.Distance(GetRivalsInformation()[0].Position, GetBallPosition());
+            float distance5 = Vector3.Distance(GetRivalsInformation()[1].Position, GetBallPosition());
+            return distance < distance2 && distance < distance3 && distance < distance4 && distance < distance5;
         }
 
         public Vector3 GetPos()
         {
+            var x = GetMyGoalPosition()[0] > 0 ? 10 : -10;
             var ballPosition = GetBallPosition();
-            Vector3 origin = new Vector3(-10.0f, 0.0f, 0.0f);
+            Vector3 origin = new Vector3(x, 0.0f, 0.0f);
             var radius = 3;
 
             Vector3 originToBall = ballPosition - origin;
@@ -26,22 +32,51 @@ namespace Teams.ORTcoderMate
             return posFinal;
         }
 
+
+        public bool CanShoot(Vector3 position, float maxDistance)
+        {
+            var startingPoint = GetPosition();
+            var direction = GetDirectionTo(position);
+            foreach (var player in GetRivalsInformation())
+            {
+                Vector3 point = player.Position;
+                Ray ray = new Ray(startingPoint, Vector3.Normalize(direction));
+                float distance = Vector3.Cross(ray.direction, point - ray.origin).magnitude;
+                if (distance < maxDistance) return false;
+            }
+            return true;
+        }
+
         public override void OnUpdate()
         {
-            if (IsBallNearOwnGoal())
-            {
+            if (IsNearest() && Vector3.Distance(GetBallPosition(), GetMyGoalPosition()) < 4.0f) {
                 GoTo(GetBallPosition());
-            }
-            else
-            {
+            } else {
                 MoveBy(GetDirectionTo(GetPos()));
             }
         }
 
         public override void OnReachBall()
         {
-            
-            ShootBall(GetDirectionTo(GetTeamMatesInformation()[1].Position), ShootForce.High);
+            if (CanShoot(GetTeamMatesInformation()[0].Position, 0.5f)) {
+                ShootBall(GetDirectionTo(GetTeamMatesInformation()[0].Position), ShootForce.High);
+                Debug.Log("Goalie: Pase a Messi");
+            } else if (CanShoot(GetTeamMatesInformation()[1].Position, 0.5f)) {
+                ShootBall(GetDirectionTo(GetTeamMatesInformation()[1].Position), ShootForce.Medium);
+                Debug.Log("Goalie: Pase a Mid");
+            } else if (CanShoot(GetRivalGoalPosition(), 1.0f)) {
+                ShootBall(GetDirectionTo(GetRivalGoalPosition()), ShootForce.High);
+                Debug.Log("Goalie: Tiro al Arco");
+            } else {
+                int x = GetMyGoalPosition()[0] > 0 ? -10 : 10;
+                int z = 10;
+                Vector3 newPos = new Vector3(x, 0, z);
+                if (CanShoot(newPos, 2.0f)) {
+                    ShootBall(GetDirectionTo(newPos), ShootForce.High);
+                } else {
+                    ShootBall(GetDirectionTo(new Vector3(-x, 0, z)), ShootForce.Medium);
+                }
+            }
         }
 
         public override void OnScoreBoardChanged(ScoreBoard scoreBoard)
@@ -51,6 +86,6 @@ namespace Teams.ORTcoderMate
 
         public override FieldPosition GetInitialPosition() => FieldPosition.A2;
 
-        public override string GetPlayerDisplayName() => "Player 1";
+        public override string GetPlayerDisplayName() => "Goalie";
     }
 }
